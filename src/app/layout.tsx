3b6dbf64c31823e06e8328305a943f9132f89f67
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2024-10-26 20:38:45 +0545

    clerk added for authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -24,12 +31,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
         {children}
       </body>
     </html>
+        </ClerkProvider>
   );
 }
