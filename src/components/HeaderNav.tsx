bc980dd09a0d51dc9ffc8499f30be17e3d5eb8b9
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2024-10-27 18:20:57 +0545

    clerk loaded and loading spinner added

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import HeaderNavButton from "./HeaderNavButton";
 import { usePathname } from "next/navigation";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { LoaderCircle } from 'lucide-react';
 const navItems = [
   {
     link: "/",
@@ -28,10 +30,20 @@ const navItems = [
 const HeaderNav = () => {
   const pathname = usePathname();
   return (
-    <nav className=" hidden lg:flex gap-4 px-14 ">
+    <nav className=" hidden w-full lg:flex px-14 justify-end ">
+      <div className="mx-auto flex gap-4">
+
       {navItems.map((item) => (
           <HeaderNavButton label={item.title} href={item.link} isActive={pathname === item.link} />
       ))}
+      </div>
+      <ClerkLoaded>
+
+      <UserButton />
+      </ClerkLoaded>
+      <ClerkLoading>
+      <LoaderCircle strokeWidth={1.5} className=" animate-spin text-white" />
+      </ClerkLoading>
     </nav>
   );
 };
--- src/components/LogoBrand.tsx
+++ src/components/LogoBrand.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import React from 'react'
-
+import logo from '../assets/logo.svg'
 const LogoBrand = () => {
   return (
     <div className=' flex gap-3 items-center'>
-        <Image src={require('../assets/logo.svg')} alt="logo" width={40} height={40} />
+        <Image src={logo} alt="logo" width={40} height={40} />
         <p className=' text-white font-semibold text-2xl '>Finance </p>
     </div>
   )
